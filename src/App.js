5b20b5c85192b42a17691397f5408af5f00b3d64
Sean OReilly <soreilly424@gmail.com>
2021-01-16 11:20:17 -0500

    lazy loading for React 16 plus using suspense

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component, Suspense } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+
+import styles from './App.module.css'
+import User from './containers/User';
+import Welcome from './containers/Welcome';
+// import Posts from './containers/Posts';
+const Posts = React.lazy(() => import('./containers/Posts'))
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <nav>
+            <NavLink to="/user">User Page</NavLink> |&nbsp;
+            <NavLink to="/posts">Posts Page</NavLink>
+          </nav>
+          <Route path="/" component={Welcome} exact />
+          <Route path="/user" component={User} />
+          <Route path="/posts" render={() => (
+          <Suspense fallback={<div className={styles.Loader}>Loading...</div>}>
+            <Posts/>
+          </Suspense>
+          )}
+        />
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
